d7e3e660287565b2456a19069169060499041bd8
root <root@ip-172-31-15-79.ap-south-1.compute.internal>
2018-11-22 17:00:57 +0000

    from ec2

--- QUIZ/server/db-conn.js
+++ QUIZ/server/db-conn.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'Root@1234',
     database: 'ASHRAMA_QUIZ'
 });
 
